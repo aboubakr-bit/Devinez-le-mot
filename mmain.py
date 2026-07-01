3155181e3270670eb72f1a8b3a83fdb433e2d9b7
aboubakr <jakoirblak@gmail.com>
2026-07-01 13:48:01 +0100

    Update mmain.py

--- mmain.py
+++ mmain.py
@@ -1,23 +1,95 @@
+hangman_stages = [
+    """
+     +---+
+     |   |
+     O   |
+    /|\\  |
+    / \\  |
+    =========
+    """, 
+    """
+     +---+
+     |   |
+     O   |
+    /|\\  |
+    /    |
+    =========
+    """,
+    """
+     +---+
+     |   |
+     O   |
+    /|\\  |
+         |
+    =========
+    """, 
+    """
+     +---+
+     |   |
+     O   |
+    /|   |
+         |
+    =========
+    """, 
+    """
+     +---+
+     |   |
+     O   |
+     |   |
+         |
+    =========
+    """, 
+    """
+     +---+
+     |   |
+     O   |
+         |
+         |
+    =========
+    """, 
+    """
+     +---+
+     |   |
+         |
+         |
+         |
+    =========
+    """  
+]
 import random
-mot = ["esquiver", "surgir", "dissimuler", "entamer", "prolonger", "déceler", "entrecroiser", "persister", "susciter", "entraver"]
+mot = ["esquiver", "surgir", "dissimuler", "entamer", "prolonger", "déceler", "entrecroiser",
+"persister", "susciter", "entraver"]
 import_choise = random.choice(mot)
+
 display = list("_" * len(import_choise))
+print(hangman_stages[6])
 print(" ".join(display))
+
 essais = 6
+lettres_devinees = []
 while "_" in display and essais > 0:
     guessed = input("Devinez une lettre: ").lower()
-    letter_found = False
+    if guessed in lettres_devinees:
+        print(f"Vous avez déjà deviné la lettre '{guessed}' ! Vos essais restent {essais}.")
+        print(hangman_stages[essais])
+        print(" ".join(display))
+        continue
+    lettres_devinees.append(guessed)
+
+    if guessed not in import_choise:
+        essais -= 1
+        print(f"Choisissez Fausse ! Il vous reste {essais} essais.")
+    else:
+        print(f"Choisissez correct ! Il vous reste {essais} essais.")
+
+    
     for position in range(len(import_choise)):
         leterr = import_choise[position]
         if leterr == guessed:
             display[position] = guessed
-            letter_found = True
-    if not letter_found:
-        essais -= 1 
-        print(f"Choisissez Fausse ! Il vous reste {essais} essais.")
-    else:
-        print(f"Choisissez correct ! Il vous reste {essais} essais.")
+    print(hangman_stages[essais])
     print(" ".join(display))
+
 if "_" not in display:
     print("""          **********
           tu gagnes
@@ -25,9 +97,9 @@ if "_" not in display:
 else:
     print(f"Tu as perdu! Le mot était: {import_choise}")
     print("""
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |""")
+      +---+
+      |   |
+      O   |
+     /|\  |
+     / \  |
+          |""")
